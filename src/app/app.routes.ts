ff3fa1b20778737c916d3bae25212301b97c0887
fjguarde <fjguarde@gmail.com>
2024-02-18 15:36:29 +0100

    feat: user list page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: AppComponent},
+    {  path: 'users', 
+      loadComponent: () => import('./pages/user-list-page/user-list-page.component').then((c) => c.UserListPageComponent) 
+    },
+];
---
+++ src/app/pages/user-list-page/user-list-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'gico-user-list-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-list-page.component.html',
+  styleUrls: ['./user-list-page.component.scss']
+})
+export class UserListPageComponent {
+
+}
