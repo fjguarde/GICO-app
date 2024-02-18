4761fc63a10d3185efabc7cc5a43890614cd9bc0
fjguarde <fjguarde@gmail.com>
2024-02-18 15:43:13 +0100

    fix: router outlet

--- src/app/pages/user-list-page/user-list-page.component.ts
+++ src/app/pages/user-list-page/user-list-page.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'gico-user-list-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './user-list-page.component.html',
   styleUrls: ['./user-list-page.component.scss']
 })
