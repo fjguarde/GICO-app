5a110dbfaa33ab26fd8b8ae04857abdfb7bb815e
fjguarde <fjguarde@gmail.com>
2024-02-18 16:04:05 +0100

    feat: table initial config

--- src/app/components/users-table/users-table.component.ts
+++ src/app/components/users-table/users-table.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { Component, Input } from '@angular/core';
+import { TableModule } from 'primeng/table';
+const PRIME_NG_MODULES = [TableModule]
 @Component({
   selector: 'gico-users-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [PRIME_NG_MODULES],
   templateUrl: './users-table.component.html',
   styleUrls: ['./users-table.component.scss']
 })
 export class UsersTableComponent {
+  @Input() users: any[] = [];
 
 }
