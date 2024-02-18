e2a7e5c8f4bb5c67c83c78fa8f52dda55a3e0182
fjguarde <fjguarde@gmail.com>
2024-02-18 17:30:19 +0100

    feat: user api service and environments

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { environment } from '@environments/environment';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(private readonly http: HttpClient) { }
+
+
+  public getUsers(filters?: any): Observable<User[]> {
+    return this.http.get<User[]>(`${environment.apiUrl}/users`, {
+      params: { ...filters }
+    })
+  }
+
+  public getUserById(userId: string): Observable<User> {
+    return this.http.get<User>(`${environment.apiUrl}/user/${userId}`)
+  }
+}
