93c6afca26dde9c226c18e9e6d6d79a0fde6452f
fjguarde <fjguarde@gmail.com>
2024-02-22 13:16:51 +0100

    feat: not found icon and styles

--- src/app/pages/not-found-page/not-found-page.component.ts
+++ src/app/pages/not-found-page/not-found-page.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'gico-not-found-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './not-found-page.component.html',
   styleUrl: './not-found-page.component.scss'
 })
